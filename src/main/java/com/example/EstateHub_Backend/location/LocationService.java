63905c667fbe6d2cc2703cde614b7bdeeceb7aa1
CodeWithAbhayraj <abhaykonge26@gmail.com>
2026-08-17 10:49:47 +0530

    update  property  module

--- src/main/java/com/example/EstateHub_Backend/location/LocationService.java
+++ src/main/java/com/example/EstateHub_Backend/location/LocationService.java
@@ -21,7 +21,6 @@ public class LocationService {
     private final AreaRepository areaRepository;
     private final PropertyTypeRepository propertyTypeRepository;
 
-
     // =====================================================
     // CITY
     // =====================================================
