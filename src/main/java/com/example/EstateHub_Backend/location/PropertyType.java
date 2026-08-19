29b189e8ec9649ceea54a9250357026f3a6d0fa0
CodeWithAbhayraj <abhaykonge26@gmail.com>
2026-08-19 20:44:05 +0530

    update property  module

--- src/main/java/com/example/EstateHub_Backend/location/PropertyType.java
+++ src/main/java/com/example/EstateHub_Backend/location/PropertyType.java
@@ -24,6 +24,7 @@ public class PropertyType {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Column(nullable = false, length = 100)
     private String name;
 
