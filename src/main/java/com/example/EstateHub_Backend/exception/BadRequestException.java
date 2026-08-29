9bd7bf26a20e2a48e7874e4ce5dc5c2734b22b86
CodeWithAbhayraj <abhaykonge26@gmail.com>
2026-08-29 15:46:42 +0530

    update

--- src/main/java/com/example/EstateHub_Backend/exception/BadRequestException.java
+++ src/main/java/com/example/EstateHub_Backend/exception/BadRequestException.java
@@ -2,7 +2,8 @@ package com.example.EstateHub_Backend.exception;
 
 public class BadRequestException extends RuntimeException {
 
-    public BadRequestException(String message) {
+    public BadRequestException(String message)
+    {
         super(message);
     }
 }
